82a577323b021258b5bdabe8db8694331fbf2f43
karem caetano <31015509+karemcaetano24@users.noreply.github.com>
2020-09-11 10:55:05 -0300

    Delete questao_4 (1).py

--- questao_4 (1).py
+++
@@ -1,5 +0,0 @@
-# Le numero de vezes que o incomoda será repetido
-n_incomoda = int(input())
-
-# O ' '.join faz a concatenação dos incomoda 
-print(f"{' '.join(['incomoda' for i in range(n_incomoda)])} muito mais")
